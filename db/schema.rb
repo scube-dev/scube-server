18b254e3d19db584637fd1ccf1d2990a03491b69
Thibault Jouan <tj@a13.fr>
2011-08-04 23:02:40 +0000

    Use Active Record session store

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,23 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110726101228) do
+ActiveRecord::Schema.define(:version => 20110804225816) do
 
   create_table "playlists", :force => true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "tracks", :force => true do |t|
     t.string   "name"
