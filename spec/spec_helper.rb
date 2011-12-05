a36310918c75e26b9ca31d6fa50c8c7b49df409d
Thibault Jouan <tj@a13.fr>
2011-12-05 18:07:46 +0000

    Fix syntax in spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,7 @@ Spork.prefork do
   require File.expand_path('../../config/environment', __FILE__)
   require 'rspec/rails'
 
-  Dir[Rails.root.join('spec/support/**/*.rb')].each {|f| require f}
+  Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
   RSpec.configure do |config|
     config.mock_with :rspec
