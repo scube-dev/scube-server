e940b3f246004f6d94f56da799872b389b44e932
Thibault Jouan <tj@a13.fr>
2015-05-03 22:52:26 +0000

    Refactor tracks specs

--- spec/integration/api/tracks_spec.rb
+++ spec/integration/api/tracks_spec.rb
@@ -1,29 +1,29 @@
 describe 'API tracks' do
   include AcceptanceHelpers
 
-  before { api_sign_in }
+  let(:track)       { create :track_with_sound }
+  let(:other_track) { create :track }
+
+  before { api_sign_in and track and other_track }
 
   it 'lists tracks' do
-    track_1 = create :track_with_sound, name: 'Track 1'
-    track_2 = create :track, name: 'Track 2'
     get api_tracks_path, format: :json
     expect(json).to eq(
       tracks: [
         {
-          id:         track_1.id,
-          name:       'Track 1',
-          sound_url:  api_sound_url(track_1.sound)
+          id:         track.id,
+          name:       track.name,
+          sound_url:  api_sound_url(track.sound)
         },
         {
-          id:         track_2.id,
-          name:       'Track 2'
+          id:         other_track.id,
+          name:       other_track.name
         }
       ]
     )
   end
 
   it 'shows a track' do
-    track = create :track
     get api_track_path track, format: :json
     expect(json).to eq(
       track: {
