50c57e8bbd513f3ca1f97d2b19cc027efe3e7fed
Thibault Jouan <tj@a13.fr>
2015-05-05 00:59:29 +0000

    Ensure HEAD on API sounds/show behave correctly

--- spec/integration/api/sounds_spec.rb
+++ spec/integration/api/sounds_spec.rb
@@ -2,9 +2,10 @@ describe 'API sounds' do
   before { api_sign_in }
 
   describe 'sound show' do
-    let(:sound)         { create :sound }
-    let(:request_show)  { get api_sound_path sound }
-    subject             { response }
+    let(:sound)           { create :sound }
+    let(:request_method)  { :get }
+    let(:request_show)    { send request_method, api_sound_path(sound) }
+    subject               { response }
 
     before { request_show }
 
@@ -16,6 +17,16 @@ describe 'API sounds' do
 
     it 'returns the sound file as the body' do
       expect(response.body).to eq File.read(sound.path, mode: 'rb')
+    end
+
+    context 'when method is HEAD' do
+      let(:request_method) { :head }
+
+      it { is_expected.to have_http_status 200 }
+
+      it 'returns an empty body' do
+        expect(response.body).to be_empty
+      end
     end
   end
 end
