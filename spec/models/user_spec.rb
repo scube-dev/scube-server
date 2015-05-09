2dd36e05819144c04e07becdc7846aa3b0cfe694
Thibault Jouan <tj@a13.fr>
2015-05-09 18:35:22 +0000

    Refactor user specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,20 +4,9 @@ describe User do
   it { is_expected.to be_valid }
   it { is_expected.to have_many :playlists }
   it { is_expected.to validate_presence_of :email }
+  it { is_expected.to validate_uniqueness_of :email }
   it { is_expected.to have_secure_password }
   it { is_expected.to validate_length_of(:password).is_at_least 8 }
-
-  context 'when a user with the same email address already exists' do
-    let(:old_user)  { create :user, email: 'unique@example.net' }
-    subject(:user)  { build :user, email: old_user.email }
-
-    it { is_expected.not_to be_valid }
-
-    it 'has an error on email attribute' do
-      user.valid?
-      expect(user.errors[:email].size).to eq 1
-    end
-  end
 
   describe '#authenticate?' do
     context 'with a valid password' do
