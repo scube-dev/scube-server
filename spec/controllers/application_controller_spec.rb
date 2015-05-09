0a1f463a859aee367761b6f77a43df7b8bb83d97
Thibault Jouan <tj@a13.fr>
2015-05-09 14:25:24 +0000

    Improve #current_user helper specs

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -9,10 +9,17 @@ describe ApplicationController do
   end
 
   describe '#current_user' do
-    context 'when session[:user_id] is set' do
-      it 'returns the User instance from the session' do
-        session[:user_id] = user.id
+    context 'when :user_id is set in session' do
+      before { session[:user_id] = user.id }
+
+      it 'returns the related user instance' do
         expect(controller.current_user).to eq user
+      end
+    end
+
+    context 'when :user_id is not set in session' do
+      it 'returns nil' do
+        expect(controller.current_user).to be nil
       end
     end
   end
