994c0a730c5e8717f3ee2b36072ee2f344fd79d9
Thibault Jouan <tj@a13.fr>
2015-08-16 09:39:35 +0000

    Refactor API sign in spec

--- spec/integration/api/sign_in_spec.rb
+++ spec/integration/api/sign_in_spec.rb
@@ -1,28 +1,31 @@
 describe 'API sign in' do
-  let(:user) { create :user }
+  let(:user)      { create :user }
+  let(:email)     { user.email }
+  let(:password)  { user.password }
 
   def do_create
     jpost api_sessions_path, session: {
-      email:    user.email,
-      password: user.password
+      email:    email,
+      password: password
     }
   end
 
+  before { do_create }
+
   it 'returns a token' do
-    do_create
     expect(json).to match(
       session: {
-        id: an_instance_of(Fixnum),
-        token: /\A[\w\d]{24}\z/
+        id:     an_instance_of(Fixnum),
+        token:  /\A[\w\d]{24}\z/
       }
     )
   end
 
   [:email, :password].each do |attr|
     context "with invalid #{attr}" do
+      let(attr) { 'INVALID' }
+
       it 'rejects authentication' do
-        allow(user).to receive(attr).and_return(user.send(attr) + '_INVALID')
-        do_create
         expect(response).to be_not_found
         expect(response.body).to be_empty
       end
