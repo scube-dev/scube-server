ab0cf23ae54baaf1b1b989e7861655f977607eaf
Thibault Jouan <tj@a13.fr>
2015-08-16 10:03:16 +0000

    Improve parameters handling in API sessions/create

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -3,7 +3,7 @@ module API
     skip_before_filter :authenticate!, only: :create
 
     def create
-      user = User.find_by_email(params[:session][:email])
+      user = User.find_by_email(session_params[:email])
 
       unless user.try :authenticate?, params[:session][:password]
         return render json: '', status: :not_found
@@ -11,6 +11,12 @@ module API
 
       @key = user.keys.create name:
         "Login from external app `#{request.headers['Origin']}'"
+    end
+
+  private
+
+    def session_params
+      params.require(:session).permit %i[email password]
     end
   end
 end
