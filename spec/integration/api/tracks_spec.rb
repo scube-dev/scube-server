96571027cc42cbf909597a3572067ba8b80f1bd3
Thibault Jouan <tj@a13.fr>
2015-08-29 20:43:37 +0000

    Improve API tracks integration specs

--- spec/integration/api/tracks_spec.rb
+++ spec/integration/api/tracks_spec.rb
@@ -2,32 +2,38 @@ describe 'API tracks' do
   let(:track)       { create :track_with_sound }
   let(:other_track) { create :track }
 
-  before { api_sign_in and track and other_track }
+  before { api_sign_in }
 
-  it 'lists tracks' do
-    jget api_tracks_path
-    expect(json).to eq(
-      tracks: [
-        {
-          id:         track.id,
-          name:       track.name,
-          sound_url:  api_sound_url(track.sound)
-        },
-        {
-          id:         other_track.id,
-          name:       other_track.name
-        }
-      ]
-    )
+  describe 'index' do
+    before { track and other_track }
+
+    it 'lists tracks' do
+      jget api_tracks_path
+      expect(json).to eq(
+        tracks: [
+          {
+            id:         track.id,
+            name:       track.name,
+            sound_url:  api_sound_url(track.sound)
+          },
+          {
+            id:         other_track.id,
+            name:       other_track.name
+          }
+        ]
+      )
+    end
   end
 
-  it 'shows a track' do
-    jget api_track_path track
-    expect(json).to eq(
-      track: {
-        id:   track.id,
-        name: track.name
-      }
-    )
+  describe 'show' do
+    it 'shows a track' do
+      jget api_track_path track
+      expect(json).to eq(
+        track: {
+          id:   track.id,
+          name: track.name
+        }
+      )
+    end
   end
 end
