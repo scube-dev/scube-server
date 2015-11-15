7f71d8323772be2f074c82038f0f8b7561b70046
Thibault Jouan <tj@a13.fr>
2015-11-15 17:57:28 +0000

    Improve AR relation declarations in Track and User

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,9 +1,9 @@
 class Track < ActiveRecord::Base
+  has_many :authors, through: :track_authors
   has_many :release_tracks
   has_many :releases, through: :release_tracks
   has_many :sounds
   has_many :track_authors, dependent: :destroy
-  has_many :authors, through: :track_authors
 
   validates :name, presence: true
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  has_many :playlists
   has_many :keys
+  has_many :playlists
 
   validates :email,     presence: true, uniqueness: true
   validates :password,  length: { minimum: 8 }, if: :password_digest_changed?
