59935d465bb43de0a857dacb59529c23cfd91d6e
Thibault Jouan <tj@a13.fr>
2015-11-21 16:43:41 +0000

    Remove dead code from ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,6 @@ class ApplicationController < ActionController::API
 
   before_action :authenticate!
 
-  skip_before_action :verify_authenticity_token
   skip_before_action :authenticate!, only: %i[cor_preflight ping]
 
   before_action :cor_filter
