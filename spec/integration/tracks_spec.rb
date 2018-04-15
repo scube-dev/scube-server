25158e4b1acb6d19d2947e065dd192394976dd96
Thibault Jouan <tj@a13.fr>
2018-04-15 10:10:08 +0000

    Remove dead code from tracks/create spec

--- spec/integration/tracks_spec.rb
+++ spec/integration/tracks_spec.rb
@@ -65,7 +65,7 @@ RSpec.describe 'Tracks' do
     end
 
     context 'when track has a sound' do
-      let!(:track) { attributes_for :track, :with_file_upload }
+      let(:track) { attributes_for :track, :with_file_upload }
 
       it 'creates related sound' do
         expect(json[:track]).to include :sound_path
